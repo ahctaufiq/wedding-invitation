13b2a3d9fabcdf72994e2f6c1251ab83971d4667
ahcmad <ahcmadtaufiq98@gmail.com>
2025-11-22 16:44:33 +0800

    remove hubungi page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,10 @@ import { InvitationComponent } from './features/invitation/invitation.component'
 import { LokasiComponent } from './features/lokasi/lokasi.component';
 import { GuestbookComponent } from './features/guestbook/guestbook.component';
 import { RsvpComponent } from './features/rsvp/rsvp.component';
-import { HubungiComponent } from './features/hubungi/hubungi.component';
 
 export const routes: Routes = [
   { path: '', component: InvitationComponent },         // page utama
   { path: 'lokasi', component: LokasiComponent },       // 1: Lokasi
   { path: 'guestbook', component: GuestbookComponent }, // 2: Ucapan tetamu
   { path: 'rsvp', component: RsvpComponent },           // 3: RSVP
-  { path: 'hubungi', component: HubungiComponent },     // 4: Hubungi
 ];
